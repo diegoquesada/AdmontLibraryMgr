4e5cc401549390d9a3424a465889d70db5b7111a
Diego Quesada <diego.developer@me.com>
2018-12-11 18:45:57 -0500

    Added validation to Book model.

--- AdmontLibraryMgr/Models/Book.cs
+++ AdmontLibraryMgr/Models/Book.cs
@@ -9,17 +9,27 @@ namespace AdmontLibraryMgr.Models
     public class Book
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+
         public string Author { get; set; }
         public string Language { get; set; }
 
+        [Range(1000, 2500)]
+        [Display(Name = "Year of Publication")]
         public int PublishYear { get; set; }
+
         public string Publisher { get; set; }
 
         public string Subject { get; set; }
+
+        [Display(Name = "Dewey Number")]
         public string DeweyNumber { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Date Acquired")]
         public DateTime DateAcquired { get; set; }
     }
 }
